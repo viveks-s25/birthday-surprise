5d045659ddbc68dcf2ca16103c215d24e8de199e
viveks25 <dare.devil.99900@gmail.com>
2026-09-15 09:08:43 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/',
+  base: '/birthday-surprise/',
   plugins: [react(), tailwindcss()],
 })
